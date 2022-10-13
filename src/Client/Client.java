49c14672b5f5c512205f09d64b0ed88c93dd1c01
Erick02A <102835005+Erick02A@users.noreply.github.com>
2022-10-12 20:21:30 -0600

    Se crean los mains de ambas apps

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,7 +1,31 @@
 package Client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
     public static void main (String[] args){
+        final String HOST = "127.0.0.1";
+        final int PORT = 5000;
+        DataOutputStream out;
+        DataInputStream in;
 
+        try {
+            Socket clientSocket = new Socket(HOST, PORT);
+
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+
+            out.writeUTF("Hello world from client");
+
+            String message = in.readUTF();
+            System.out.println(message);
+
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,7 +1,42 @@
 package Server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Server {
     public static void main (String[] args){
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        DataInputStream in;
+        DataOutputStream out;
+        final int PORT = 5000;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Server in on!!");
 
+            while (true) {
+                clientSocket = serverSocket.accept();
+                System.out.println("Client connected");
+
+
+                in = new DataInputStream(clientSocket.getInputStream());
+                out = new DataOutputStream(clientSocket.getOutputStream());
+
+                String message = in.readUTF();
+                System.out.println(message);
+
+                out.writeUTF("Message Received");
+
+                clientSocket.close();
+                System.out.println("Client disconnected");
+            }
+        } catch (IOException e) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null,e);
+        }
     }
 }
